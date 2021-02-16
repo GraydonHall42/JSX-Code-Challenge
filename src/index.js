481c0caff0e942ac9e47d19f9a3f8e87578b80e7
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-16 10:40:09 -0700

    Challenge Completed

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>H1 Heading Element</h1>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
